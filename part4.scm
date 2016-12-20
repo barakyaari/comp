79d15f112c68ae4910daba45e038fbd3cda77503
BarakYaari <barakyaari@gmail.com>
2016-12-20 14:58:56 +0200

    Added structure for part 4

--- part4.scm
+++ part4.scm
@@ -1,3 +1,38 @@
-(display 'a)
+(define example
+  (car '((applic
+		(lambda-simple
+			(a)
+		(applic
+	(var list)
+	((lambda-simple () (var a))
+	(lambda-simple
+	()
+	(set (var a) (applic (var +) ((var a) (const 1)))))
+	(lambda-simple (b) (set (var a) (var b))))))
+  ((const 0))))))
 
-(display 2)
+
+
+(define hasBoundOccurence
+  (lambda (exp procedure)
+    (display exp)
+    #t))
+
+(define hasSet
+  (lambda (exp procedure)
+    #t))
+
+(define hasGet
+  (lambda (exp procedure)
+    #t))
+
+(define shouldReplaceVaribale
+  (lambda (param procedure)
+    (and
+      (hasBoundOccurence param procedure)
+      (hasSet param procedure)
+      (hasGet param procedure)
+      )))
+
+(shouldReplaceVaribale 'a example)
+
