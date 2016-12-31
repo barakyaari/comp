fd14ed0f7f8a1659c669314f2e4002222ab9d18e
t-baya <t-baya@microsoft.com>
2016-12-31 14:16:47 +0200

    added part 3

---
+++ part3.scm
@@ -0,0 +1,25 @@
+(define eliminate-nested-defines
+	(lambda (x) x))
+
+(define eliminate-nested-defines-and-parse
+	(lambda (exp)
+		(letrec (
+				(helper
+					(lambda(lst sets) 
+						(cond
+							((atom? lst) (cons sets '()))
+							((and
+								(list? (car lst))
+								(eq? 'define (caar lst)))
+								(helper 
+									(cdr lst)
+									(append 
+										sets 
+										(list 
+											(list (cadar lst) (caddar lst)))))
+								)
+							(else (cons sets (cdr lst)))
+						))))
+		(if (< 1 (length exp))
+			(parse `(letrec ,(car (helper exp '())) ,(cdr (helper exp '()))))
+			(parse exp)))))
