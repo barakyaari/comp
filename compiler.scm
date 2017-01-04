013c2f38300b816bc35a542165ae22d1b063f15c
Sahar <sahare92@gmail.com>
2017-01-04 14:28:59 +0200

    part3+4+compiler-changed-and-working

--- compiler.scm
+++ compiler.scm
@@ -1510,14 +1510,160 @@
 
 ;;;;;;;;;;;;   part3: Eliminate nested defines: ;;;;;;;;;;;;;;;
 
-;----- ayelet functions for testing - (to delete):
+; f == predicate? | new-item == the function to apply on the   
+(define (dynamic-map-2 f new-item lst)
+  (cond
+    ((null? lst) lst)
+    ((atom? lst) lst)    
+    (else (if (and (list? lst) (f lst))
+        (new-item (cons 
+          (dynamic-map-2 f new-item (car lst))
+          (dynamic-map-2 f new-item (cdr lst))))
+          (cons (dynamic-map-2 f new-item (car lst))
+                    (dynamic-map-2 f new-item (cdr lst)))))))
 
-;;;;;;;;;;;;;;;;;;;;; Removing redundant applications ;;;;;;;;;;;;;;;;;;;;
+(define actual-eliminate-nested-defines
+  (lambda (exp)
+    (letrec(
+        ;Receives: a body of a lambda if it starts with seq 
+        ;Returns: list of sets      
+        (find-sets
+          (lambda(body sets) 
+            (cond
+              ((atom? body) sets)
+              ((and 
+                (list? (car body))
+                (eq? 'def (caar body)))
+                (find-sets
+                    (cdr body)
+                    (append
+                      sets
+                      (list 
+                        (list
+                        ;(var <something>)
+                        (cadar body)
+                        ;(the set exp)
+                        (caddar body))))))
+              (else sets))))
+        ;Receives: a body of a lambda if it starts with seq 
+        ;Returns: body without defines          
+        (find-body
+          (lambda(body sets) 
+            (cond
+              ((atom? body) body)
+              ((and 
+                (list? (car body))
+                (eq? 'def (caar body)))
+                (find-body
+                    (cdr body)
+                    (append
+                      sets
+                      (list 
+                        (list
+                        ;(var <something>)
+                        (cadar body)
+                        ;(the set exp)
+                        (caddar body))))))
+              (else body))))          
+        (combine-into-letrec
+          (lambda (sets body)
+            (cond 
+              ((null? sets) body)
+              (else
+                (let*(
+                    (vars
+                      (map
+                        (lambda (x)
+                          (cadr (car x)))
+                        sets))
+                    (new-sets
+                      (map 
+                        (lambda (lst)
+                          `(set ,(car lst) ,(cadr lst)))
+                        sets))                    
+                    (end-constf
+                      (map 
+                        (lambda (lst)
+                          `(const #f))
+                        sets))
+                    (actual-combine-into-letrec
+                      `(applic
+                        (lambda-simple
+                          ,vars
+                          (seq ( ,@new-sets
+                            ,@body )))
+                            ,end-constf)))
+                  actual-combine-into-letrec)))))
+        );letrec-end
 
-(load "ayeletCode.scm")
+      (combine-into-letrec 
+        (find-sets exp '()) 
+        (find-body exp '())
+      ))))
 
-;;;;;;;;;;;;   part4: Remove empty lambdas: ;;;;;;;;;;;;;;;
+(define eliminate-nested-defines
+  (lambda (exp)
+    (dynamic-map-2
+      ;f
+      (lambda (lst)
+        (or
+          (and 
+            (or
+              (eq? 'lambda-simple (car lst))
+              (eq? 'lambda-var (car lst)))
+            (eq? 'seq (caaddr lst))
+            (eq? 'def (caaadr (caddr lst))))
+          (and
+            (eq? 'lambda-opt (car lst))
+            (eq? 'seq (car (cadddr lst)))
+            (eq? 'def (caaadr (cadddr lst))))))
+      ;new-item
+      (lambda (lst)
+        (if (eq? 'lambda-opt (car lst))
+          `(,(car lst) ,(cadr lst) ,(caddr lst) ,(actual-eliminate-nested-defines (car (cdr (cadddr lst)))))
+          `(,(car lst) ,(cadr lst) ,(actual-eliminate-nested-defines (car (cdaddr lst))))
+          ))
+      exp)
+    ))
 
+
+;;;;;;;;;;;;   part4: Remove redundant applications: ;;;;;;;;;;;;;;;
+
+; f == predicate? | new-item == the function to apply on the   
+(define (dynamic-map f new-item lst)
+  (cond
+    ((null? lst) lst)
+    ((atom? lst) lst)    
+    (else (if (and (list? lst) (f lst))
+        (dynamic-map f new-item (new-item lst))
+        (cons (dynamic-map f new-item (car lst))
+                  (dynamic-map f new-item (cdr lst)))))))
+
+(define remove-applic-lambda-nil
+  (lambda(exp)
+    (let* (
+        (no-pars-simple-lambda?
+          (lambda (lst)
+            (and 
+              (list? lst)
+              (eq? 'lambda-simple (car lst))
+              (null? (cadr lst))
+              )))
+        (helper-pred
+          (lambda (lst)
+            (and 
+                (list? lst)
+                (eq? 'applic (car lst))
+                (no-pars-simple-lambda? (cadr lst))
+                (null? (caddr lst)))
+              ))
+        (helper-new-item
+          (lambda (lst)
+            (caddr (cadr lst))
+            ))
+        )
+      (dynamic-map helper-pred helper-new-item exp)
+    )))
 
 ;;;;;;;;;;;;   part5: Box parameters: ;;;;;;;;;;;;;;;
 
--- part3.scm
+++ part3.scm
@@ -1,28 +1,113 @@
-(define eliminate-nested-defines
-	(lambda (x) x))
+; f == predicate? | new-item == the function to apply on the   
+(define (dynamic-map-2 f new-item lst)
+  (cond
+    ((null? lst) lst)
+    ((atom? lst) lst)    
+    (else (if (and (list? lst) (f lst))
+				(new-item (cons 
+					(dynamic-map-2 f new-item (car lst))
+					(dynamic-map-2 f new-item (cdr lst))))
+	    		(cons (dynamic-map-2 f new-item (car lst))
+	            	    (dynamic-map-2 f new-item (cdr lst)))))))
 
-(define eliminate-nested-defines-and-parse
+(define actual-eliminate-nested-defines
 	(lambda (exp)
-		(letrec (
-				(helper
-					(lambda(lst sets) 
+		(letrec(
+				;Receives: a body of a lambda if it starts with seq	
+				;Returns: list of sets			
+				(find-sets
+					(lambda(body sets) 
 						(cond
-							((atom? lst) (cons sets '()))
-							((and
-								(list? (car lst))
-								(eq? 'define (caar lst)))
-								(helper 
-									(cdr lst)
-									(append 
-										sets 
-										(list 
-											(list (cadar lst) (caddar lst)))))
-								)
-							(else (cons sets (cdr lst)))
-						))))
-		(if (< 1 (length exp))
-			(parse `(letrec ,(car (helper exp '())) ,(cdr (helper exp '()))))
-			(parse exp)))))
+							((atom? body) sets)
+							((and 
+								(list? (car body))
+								(eq? 'def (caar body)))
+								(find-sets
+									 	(cdr body)
+									 	(append
+									 		sets
+									 		(list 
+									 			(list
+									 			;(var <something>)
+									 			(cadar body)
+									 			;(the set exp)
+									 			(caddar body))))))
+							(else sets))))
+				;Receives: a body of a lambda if it starts with seq	
+				;Returns: body without defines					
+				(find-body
+					(lambda(body sets) 
+						(cond
+							((atom? body) body)
+							((and 
+								(list? (car body))
+								(eq? 'def (caar body)))
+								(find-body
+									 	(cdr body)
+									 	(append
+									 		sets
+									 		(list 
+									 			(list
+									 			;(var <something>)
+									 			(cadar body)
+									 			;(the set exp)
+									 			(caddar body))))))
+							(else body))))					
+				(combine-into-letrec
+					(lambda (sets body)
+						(cond 
+							((null? sets) body)
+							(else
+								(let*(
+										(vars
+											(map
+												(lambda (x)
+													(cadr (car x)))
+												sets))
+										(new-sets
+											(map 
+												(lambda (lst)
+													`(set ,(car lst) ,(cadr lst)))
+												sets))										
+										(end-constf
+											(map 
+												(lambda (lst)
+													`(const #f))
+												sets))
+										(actual-combine-into-letrec
+											`(applic
+												(lambda-simple
+													,vars
+													(seq ( ,@new-sets
+														,@body )))
+														,end-constf)))
+									actual-combine-into-letrec)))))
+				);letrec-end
 
+			(combine-into-letrec 
+				(find-sets exp '()) 
+				(find-body exp '())
+			))))
 
-(eliminate-nested-defines-and-parse '((define a 2)(define b 3)))
+(define eliminate-nested-defines
+	(lambda (exp)
+		(dynamic-map-2
+			;f
+			(lambda (lst)
+				(or
+					(and 
+						(or
+							(eq? 'lambda-simple (car lst))
+							(eq? 'lambda-var (car lst)))
+						(eq? 'seq (caaddr lst)))
+					(and
+						(eq? 'lambda-opt (car lst))
+						(eq? 'seq (caadddr lst)))))
+			;new-item
+			(lambda (lst)
+				(if (eq? 'lambda-opt (car lst))
+					`(,(car lst) ,(cadr lst) ,(caddr lst) ,(actual-eliminate-nested-defines (car (cdadddr lst))))
+					`(,(car lst) ,(cadr lst) ,(actual-eliminate-nested-defines (car (cdaddr lst))))
+					))
+			exp)
+		))
