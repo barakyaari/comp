25f6ded26566f0a8c68f6cba38ea016c630a1c98
BarakYaari <barakyaari@gmail.com>
2017-01-11 18:18:56 +0200

    part4

--- combine.scm
+++
@@ -1,8 +0,0 @@
-(load "compiler.scm")
-;(load "part3.scm")
-;(load "part4.scm")
-(load "part5.scm")
-;(load "part6.scm")
-;(load "part7.scm")
-
-;(eliminate-nested-defines-and-parse '((define a 2)(define b 3) 'body + 1 2))
