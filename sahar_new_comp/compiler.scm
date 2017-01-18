e850d72c93d217a0602d83a44fd129eab3325996
Sahar <sahare92@gmail.com>
2017-01-18 12:42:02 +0200

    Working on changes

--- sahar_new_comp/compiler.scm
+++ sahar_new_comp/compiler.scm
@@ -34,11 +34,33 @@
 (define makePrologue
 	(lambda ()
 		(string-append 
-			"Prologue"
+"
+#include <stdio.h>
+#include <stdlib.h>
+#include \"arch/cisc.h\"
+int main() {
+START_MACHINE;
+JUMP(PROGRAM_START);
+#include \"arch/char.lib\"
+#include \"arch/io.lib\"
+#include \"arch/scheme.lib\"
+#include \"arch/math.lib\"
+#include \"arch/string.lib\"
+#include \"arch/system.lib\"
+ERROR:
+HALT;
+PROGRAM_START:
+
+"
 			printNewLine)))
 
 (define makeEpilogue
 	(lambda ()
 		(string-append 
-			"Epilogue"
+"
+DROP(IMM(3));
+STOP_MACHINE;
+HALT;
+}
+"
 			printNewLine)))
