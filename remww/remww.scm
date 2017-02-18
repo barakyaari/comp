a17aae42111a31da62c2410c2246bcb49dd7268a
Sahar <sahare92@gmail.com>
2017-02-18 13:30:43 +0200

    Save

--- remww/remww.scm
+++ remww/remww.scm
@@ -1,9 +1,25 @@
+(define removeEmptyInstsAndAddSets
+	(lambda (instList)
+		(let*
+			(
+				(getInst car)
+				(getReadRegs cadr)
+				(getWriteRegs caddr)				
+				(filteredEmptyWrites (filter (lambda (inst) (not (null? (getWriteRegs inst)))) instList))
+			)
+			(map (lambda (inst) (list (getInst inst) (getReadRegs inst) (getWriteRegs inst) '())) filteredEmptyWrites)
+			)
+		))
+
 (define remww 
 	(lambda (instList)
-		(let*(
-				(inst car)
-				(readRegs cadr)
-				(writeRegs caddr)
+		(let*
+			(
+				(getInst car)
+				(getReadRegs cadr)
+				(getWriteRegs caddr)
+				(getSet caddr)	
+				(processedInstList (removeEmptyInstsAndAddSets instList))
 			)
-			(map (lambda (x) (writeRegs x)) instList)
+			processedInstList
 		)))
